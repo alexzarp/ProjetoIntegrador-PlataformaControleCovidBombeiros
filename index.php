73e227aa025b116416ea2685bafd648925f7cdc8
Alex Sandro <alexszds@gmail.com>
2021-04-24 13:10:10 -0300

    Tela de fundo principal

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- O index é a tela de login -->
+<!-- Isso será mudado posteriormente -->
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -9,7 +10,7 @@
     <meta name="description" content="Plataforma de controle Covid-19">
     <meta name="keywords" content="Bombeiros, SC, controle Covid-19, Bombeiros Chapecó">
     <link rel="shorcut icon" href="assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png">
-    <link rel="stylesheet" href="assets/CSS/index.css">
+    <link rel="stylesheet" href="assets/CSS/login.css">
     <link rel="stylesheet" href="assets/CSS/geral.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 </head>
---
+++ view/layout/fundo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt=br">
+    <head>
+        <!-- <title><?php $titulo?></title> Precisa ser dinâmico-->
+        <meta charset="utf-8">
+        <meta name="author" content="Alex Sandro Zarpelon, Bruna Gabriela Disner">
+        <meta name="description" content="Plataforma de controle Covid-19">
+        <meta name="keywords" content="Bombeiros, SC, controle Covid-19, Bombeiros Chapecó">
+        <link rel="shorcut icon" href="../../assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png">
+        <link rel="stylesheet" href="../../assets/CSS/geral.css">
+        <link rel="stylesheet" href="../../assets/CSS/login.css">
+        <!--Todos os css vão precisar se linkados aqui, todos!-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    </head>
+
+    <body>
+        <main>
+        <img id="logo" src="../../assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png" alt="Logo BOmbeiros SC">
+        
+        <div>
+            <!--Aqui vai o conteúdo dinâmico-->
+        </div>
+
+        <!--Este espaço ficará vazio na em modo coluna 15 70 15-->
+        </main>
+    </body>
+</html>
