a792de17506a111d4268e50174231c7fb768bb45
Brunadisner <bruna.disner@gmail.com>
2021-04-24 14:57:48 -0300

    Tela pre-testagem

---
+++ view/pretestagem.php
@@ -0,0 +1,31 @@
+<div class="preTestagem">
+    <h1>Pré-Testagem</h1>    
+    <form>
+        <div>
+            <label for="id_nome">Nome:</label>
+            <input type="text" id="nome" name="nome"  
+            placeholder="Nome">
+        </div>
+        <br>
+        <div>
+            <label for="id_data">Data de início dos sintomas:</label>
+            <input type="date" id="data" name="data" placeholder="Data de início dos sintomas">
+        </div>
+        <div>
+            <label for="id_sintomas">Descreva seus sintomas:</label>
+            <textarea id="sintomas"></textarea>
+        </div>
+        <div>
+            <label>Realizou a vacina da covid-19</label>
+            <label><input type="radio" name="sim" value="sim">Sim</label>
+            <label><input type="radio" name="nao" value="nao">Não</label>
+        </div>
+        
+        
+    </form>
+
+    <div>
+        <a href="">Fazer cadastro</a>
+        <a href="">Recuperar senha</a>
+    </div>
+</div>
