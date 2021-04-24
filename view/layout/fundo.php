717784babea8db18a71087cf9f297b148b2382b0
Alex Sandro <alexszds@gmail.com>
2021-04-24 13:33:25 -0300

    Alterações

--- view/layout/fundo.php
+++ view/layout/fundo.php
@@ -9,19 +9,23 @@
         <link rel="shorcut icon" href="../../assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png">
         <link rel="stylesheet" href="../../assets/CSS/geral.css">
         <link rel="stylesheet" href="../../assets/CSS/login.css">
+        <link rel="stylesheet" href="../../assets/CSS/pretestagem.css">
         <!--Todos os css vão precisar se linkados aqui, todos!-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     </head>
 
     <body>
         <main>
-        <img id="logo" src="../../assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png" alt="Logo BOmbeiros SC">
-        
-        <div>
+            <img id="logo" src="../../assets/images/Logotipo_de_marca_do_Corpo_de_Bombeiros_Militar_de_Santa_Catarina.png" alt="Logo BOmbeiros SC">
+            
+            <div>
             <!--Aqui vai o conteúdo dinâmico-->
-        </div>
+                <?php
+                    include_once ("../pretestagem.php");
+                ?>
+            </div>
 
-        <!--Este espaço ficará vazio na em modo coluna 15 70 15-->
+            <!--Este espaço ficará vazio na em modo coluna 15 70 15-->
         </main>
     </body>
 </html>
---
+++ view/pretestagem.php
@@ -0,0 +1,21 @@
+<div class="preTestagem">
+    <h1>Pré-Testagem</h1>    
+    <form>
+        <div>
+            <label for="id_nome">Nome:</label>
+            <input type="text" id="nome" name="nome" size="50" maxlength="50" 
+            placeholder="Nome">
+        </div>
+        <br>
+        <div>
+            <input type="date" id="data" name="data" placeholder="Data de início dos sintomas">
+        </div>
+        
+        <input type="submit" value="Acesso" id="submeter"><br>
+    </form>
+
+    <div>
+        <a href="">Fazer cadastro</a>
+        <a href="">Recuperar senha</a>
+    </div>
+</div>
