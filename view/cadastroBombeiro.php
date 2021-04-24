f10e375d5d9cce419d428c16b53b228abb13e865
Alex Sandro <alexszds@gmail.com>
2021-04-24 14:54:09 -0300

    Cadastro de bombeiro no sistema view

---
+++ view/cadastroBombeiro.php
@@ -0,0 +1,13 @@
+<div>
+    <form>
+        <h1>Cadastro do Bombeiro no sistema</h1><br>
+
+        <input type="text" id="nome" name="nome" placeholder="Nome completo do militar"><br>
+        <input type="text" id="matricula" name="matricula" placeholder="Matrícula"><br>
+        <input type="text" id="email" name="email" placeholder="E-mail para recebimento de notificações"><br>
+        <input type="password" id="senha" name="senha" placeholder="Crie uma senha"><br>
+        <input type="password" id="re_senha" name="re_senha" placeholder="Repita a senha"><br>
+
+        <input type="submit" value="Submeter cadastro" id="submeter">
+    </form>
+</div>
