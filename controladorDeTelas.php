806749281d790344c22385a9ffb9190a12432efe
Alex Sandro <alexszds@gmail.com>
2021-05-10 15:47:35 -0300

    Cadastro de usuário funcional

--- controladorDeTelas.php
+++ controladorDeTelas.php
@@ -31,6 +31,11 @@ else {
             $_SESSION['caminhoCSS'] = 'assets/CSS/cadastroBombeiro.css';
             $_SESSION['caminhoDeFundo'] = 'view/cadastroBombeiro.php';
             include ("view/layout/fundo.php");
+
+            if (isset($_POST['email']) && isset($_POST['matricula']) && isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['re_senha'])) {
+                $b->cadastroBombeiro( $_POST['nome'], $_POST['matricula'], $_POST['email'], md5($_POST['senha']), md5($_POST['re_senha']));
+                echo "<strong id='inserido'>Cadastro feito com sucesso!</strong>";
+            }
         break;
 
         case 'acompanhamento':
--- view/cadastroBombeiro.php
+++ view/cadastroBombeiro.php
@@ -1,9 +1,9 @@
 <div>
-    <form method="POST" onsubmit="return validateForm()">
+    <form method="POST" onsubmit="return validateForm()" action="controladorDeTelas.php?acao=cadastro_bombeiro">
         <h1>Cadastro do Bombeiro no sistema</h1><br>
 
         <input type="text" id="nome" name="nome" placeholder="Nome completo do militar"><br>
-        <input type="text" id="matricula" name="matricula" placeholder="Matrícula no formato 123-4"><br>
+        <input type="text" id="matricula" name="matricula" placeholder="Matrícula no formato 123456-7"><br>
         <input type="text" id="email" name="email" placeholder="E-mail para recebimento de notificações"><br>
         <input type="password" id="senha" name="senha" placeholder="Crie uma senha"><br>
         <input type="password" id="re_senha" name="re_senha" placeholder="Repita a senha"><br>
