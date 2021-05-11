dd8abdc4641e7ab3a8a54a69906d267a51f04112
Brunadisner <bruna.disner@gmail.com>
2021-05-10 22:38:34 -0300

    tentando fazer a validação em js

--- assets/JS/function.js
+++ assets/JS/function.js
@@ -1,11 +1,10 @@
 
 function validateForm(){
-    // alert('teste');
     var nome = document.getElementById("nome").value;
     var email = document.getElementById("email").value;
-    var sintomas = document.getElementById("sintomas").value;
-    var matricula = document.getElementById("matricula").value;
-    var data = document.getElementById("data").value;
+    //var sintomas = document.getElementById("sintomas").value;
+    //var matricula = document.getElementById("erro").value;
+    //var data = document.getElementById("data").value;
     var senha = document.getElementById("senha").value;
     var confirma_senha = document.getElementById("re_senha").value;
     
@@ -17,19 +16,7 @@ function validateForm(){
         alert("O email deve ser preenchido");
         return false;
     }
-    if (sintomas == null || sintomas == "" || sintomas.indexOf(" ") == -1){
-        alert("Os sintomas devem ser preenchidos");
-        return false;
-    }
-    if (data == null || data == "" || data.indexOf(" ") == -1){
-        alert("A data deve ser prenchida");
-        return false;
-    }
-    
-    if (matricula == null || matricula == "" || matricula.indexOf(" ") == -1){
-        alert("A matricula deve ser preenchida");
-        return false;
-    }
+   
     
     if(senha != "" && confirma_senha != "" && senha === confirma_senha){
         return true;
@@ -41,11 +28,22 @@ function validateForm(){
         
     
 }
+var specialKeys = new Array();
+specialKeys.push(8);
+function numero(e){
+    var keyCode = e.which ? e.which : e.keyCode
+    var ret = ((keyCode >= 10 && keyCode <= 57) || specialKeys.indexOf(keyCode) != -1);
+    document.getElementById("error");
+    
+    return ret;
+    
+    
+}
+
 
 function validateForm2(){
     var nome = document.getElementById("nome").value;
     var sintomas = document.getElementById("sintomas").value;
-    var data = document.getElementById("data").value;
     if (sintomas == null || sintomas == "" || sintomas.indexOf(" ") == -1){
         alert("Os sintomas devem ser preenchidos");
         return false;
@@ -54,6 +52,34 @@ function validateForm2(){
         alert("O nome completo deve ser preenchido");
         return false;
     }
+}
+
+function validateForm3(){
+    var nome = document.getElementById("nome").value;
+    var sintomas = document.getElementById("sintomas").value;
+    var radio = document.getElementsByName("retorno");
+    escolhe = -1;
+    for (var i = radio.length -1; i > -1; i--) {
+        if (radio[i].checked) {
+            escolhe = i;
+        }
+    }
+    if(escolhe==-1){
+        alert("Por favor escolha uma opção");
+        return false;
+    }
+    if (sintomas == null || sintomas == "" || sintomas.indexOf(" ") == -1){
+        alert("Os sintomas devem ser preenchidos");
+        return false;
+        
+    }
+    if (nome == null || nome == "" || nome.indexOf(" ") == -1){
+        alert("O nome completo deve ser preenchido");
+        return false;
+    }
+
+
+
 }
 
 
@@ -78,14 +104,41 @@ function validateForm4(){
 }
 
 function validateForm5(){
-    var data = formp.data.value;
-    var sintomas = formp.sintomas.value;
-    var vacina = formp.vacina.value;
-    var seleciona = formp.tipo.value;
-    var faixaidade = formp.idade.value;
-
-    if (data == ""){
-        alert("Preencha a data ");
+    var nome = document.getElementById("nome").value;
+    var sintomas = document.getElementById("sintomas").value;
+    var radio = document.getElementsByName("vacina");
+    var tipo = document.getElementsByName("tipo");
+    var tipo2 = document.getElementsByName("idade");
+    escolhe = -1;
+    for (var i = radio.length -1; i > -1; i--) {
+        if (radio[i].checked) {
+            escolhe = i;
+        }
+    }
+    // if(escolhe==-1){
+    //     alert("Por favor escolha uma opção");
+    //     return false;
+    // }
+    if(form.bombeiro.selectedIndex == 0 || escolhe == -1){
+        alert("Selecione um bombeiro!");
+        form.st_contrato.focus();
+        return false;
     }
 
+    if (sintomas == null || sintomas == "" || sintomas.indexOf(" ") == -1){
+        alert("Os sintomas devem ser preenchidos");
+        return false;
+    }
+    if (nome == null || nome == ""){
+        alert("O nome completo deve ser preenchido");
+        return false;
+    }
+    if (tipo.value == " " || tipo.value == "Selecione"){
+        alert("Escolha uma opção.");
+        return false;
+    }
+    if (tipo2.value == " " || tipo2.value == "Selecione"){
+        alert("Escolha uma opção.");
+        return false;
+    }
 }
