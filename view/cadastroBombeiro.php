7d08adc07e51a66b4625c1d47cef7af2656e0c4d
Brunadisner <bruna.disner@gmail.com>
2021-05-11 15:01:36 -0300

    arrumando o js

--- view/cadastroBombeiro.php
+++ view/cadastroBombeiro.php
@@ -1,11 +1,11 @@
 <div>
-    <form  method="POST" onsubmit="return validateForm()">
+    <form  method="POST" onsubmit="return validateForm();validaMatricula()">
         <h1>Cadastro do Bombeiro no sistema</h1><br>
 
         <input type="text" id="nome" name="nome" placeholder="Nome completo do militar"><br>
-        <input type="text" onkeypress="return SomenteNumero(event);" ondrop="return false;" onpaste="return false;" 
-        id="error" maxlenght="7" 
-        name="matricula" placeholder="Matrícula no formato 1234567"><br>
+        <input type="text" onkeypress="return SomenteNumero(event);" 
+        ondrop="return false;" onpaste="return false;" 
+        id="error" maxlenght="7" name="matricula" placeholder="Matrícula no formato 1234567"><br>
         <input type="text" id="email" name="email" placeholder="E-mail para recebimento de notificações"><br>
         <input type="password" id="senha" name="senha" placeholder="Crie uma senha"><br>
         <input type="password" id="re_senha" name="re_senha" placeholder="Repita a senha"><br>
