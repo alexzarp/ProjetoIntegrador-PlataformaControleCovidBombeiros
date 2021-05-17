70fe4830c3f4858f2d0a52bb9fcb44efb3a1e8f0
informatica6bbm <informatica6bbm@gmail.com>
2021-05-17 16:06:32 -0300

    função de enviar emailo

---
+++ view/teste.php
@@ -0,0 +1,26 @@
+$erro = false;
+    if ( !isset( $_POST ) || empty( $_POST)){
+        $erro = 'POST está vazio.';
+    }   
+    //cria as variaveis dinamicamente
+    foreach ($_POST as $chave => $valor ){
+        //Remove as tags html e os espaços em branco
+        $$chave = trim(strip_tags($valor));
+    
+
+        if(empty ($valor)){
+            $erro = 'Existem campos em branco.';
+        }
+    }
+    if ((!isset ($email)|| ! filter_var ($email, FILTER_VALIDATE_EMAIL)) && !$erro){
+        $erro = 'Envie uma email válido.';
+    }
+    if ($erro){
+        echo $erro;
+    }else{
+        echo "<h1>Veja os dados enviados </h1>";
+
+        foreach ($_POST as $chave => $valor) {
+            echo '<br>' . $chave . '<br>:' . $valor . '<br><br>';
+        }
+    }
